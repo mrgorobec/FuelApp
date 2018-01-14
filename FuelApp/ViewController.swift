48d7d3d106918dad98aae7a8974d78321b0b6ea6
nashiluduvsudu <oleksandr.polishchuk@temy.co>
2018-01-14 13:10:54 +0200

    Initial Commit

---
+++ FuelApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FuelApp
+//
+//  Created by Alexandr Polishchuk on 1/14/18.
+//  Copyright © 2018 Alexandr Polishchuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
